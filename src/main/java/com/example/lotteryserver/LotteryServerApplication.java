46e99a31572f7d0084872006ecbb501fc294db71
jokingmoony <penubokim@gmail.com>
2022-11-12 13:31:02 +0900

    Initialize Spring Project

---
+++ src/main/java/com/example/lotteryserver/LotteryServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lotteryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryServerApplication.class, args);
+	}
+
+}
