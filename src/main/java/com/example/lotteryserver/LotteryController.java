f9fd7bc2a9b8501c83827641161192e1ecbde499
jokingmoony <penubokim@gmail.com>
2022-11-14 00:34:13 +0900

    Integrate lotteryService to controller

--- src/main/java/com/example/lotteryserver/LotteryController.java
+++ src/main/java/com/example/lotteryserver/LotteryController.java
@@ -1,17 +1,23 @@
 package com.example.lotteryserver;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LotteryController {
+
+    private final LotteryService lotteryService;
 
     @PostMapping("/api/enroll")
     public EnrollmentResult enroll(@RequestBody LotteryEnrollmentForm lotteryEnrollmentForm) {
         if (lotteryEnrollmentForm.getLotteryNumbers().size() != 6) {
             throw new RuntimeException();
         }
+        // TODO: fetch lotteryRoundId
+        lotteryService.register(1L, lotteryEnrollmentForm.getLotteryNumbers());
         return new EnrollmentResult();
     }
 }
