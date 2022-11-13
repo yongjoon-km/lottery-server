961174cb95b523ad379327c2b8840e94068bf09c
jokingmoony <penubokim@gmail.com>
2022-11-14 00:52:06 +0900

    Add javax validation for request form

--- src/main/java/com/example/lotteryserver/LotteryController.java
+++ src/main/java/com/example/lotteryserver/LotteryController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequiredArgsConstructor
 public class LotteryController {
@@ -12,10 +14,7 @@ public class LotteryController {
     private final LotteryService lotteryService;
 
     @PostMapping("/api/enroll")
-    public EnrollmentResult enroll(@RequestBody LotteryEnrollmentForm lotteryEnrollmentForm) {
-        if (lotteryEnrollmentForm.getLotteryNumbers().size() != 6) {
-            throw new RuntimeException();
-        }
+    public EnrollmentResult enroll(@Valid @RequestBody LotteryEnrollmentForm lotteryEnrollmentForm) {
         // TODO: fetch lotteryRoundId
         lotteryService.register(1L, lotteryEnrollmentForm.getLotteryNumbers());
         return new EnrollmentResult();
--- src/main/java/com/example/lotteryserver/LotteryEnrollmentForm.java
+++ src/main/java/com/example/lotteryserver/LotteryEnrollmentForm.java
@@ -2,9 +2,11 @@ package com.example.lotteryserver;
 
 import lombok.Data;
 
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class LotteryEnrollmentForm {
+    @Size(min=6, max=6)
     private List<Integer> lotteryNumbers;
 }
